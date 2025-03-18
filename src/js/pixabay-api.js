2a0d7427213bac0f29762f6da306cbab864d8763
Vladislav <139064573+worlems@users.noreply.github.com>
2025-03-18 23:10:10 +0200

    hw

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
-export const fetchImg = async (query, page = 1) => {
-  const params = new URLSearchParams({
-    key: '49253539-084e8be9ad99f0e2743df86f8',
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 15,
-    page: page,
-  });
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-  const response = await axios.get(`https://pixabay.com/api/?${params}`);
-  return response.data;
+export const fetchImg = async function (dataName, currentPage) {
+  const { data } = await axios.get('', {
+    params: {
+      key: '49253539-084e8be9ad99f0e2743df86f8',
+      q: dataName,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page: currentPage,
+    },
+  });
+  return data;
 };
